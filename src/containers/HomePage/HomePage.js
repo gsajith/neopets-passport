58d9fb87d7427f815461b0987e4ddd936b80d181
Gautham Sajith <gsajith@umich.edu>
2019-10-13 18:22:51 -0400

    Homepage progress

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,12 +1,90 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Tilt from 'react-vanilla-tilt';
+
 import './HomePage.scss';
+import WorldMap from '../../assets/imgs/neopia_world.png';
 
 const HomePage = (props) => {
   const { name } = props;
   return (
     <div>
-      {name}
+      <img src={WorldMap} alt="Neopia World" className="worldBackground" />
+      <div className="navigationContainer">
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer">
+          <Tilt className="tilt">
+            <div className="hexagon">
+              <div className="hexagon-in1">
+                <img src="http://placekitten.com/100/200" className="hexagon-in2" />
+              </div>
+            </div>
+          </Tilt>
+        </div>
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer">
+          <Tilt className="tilt">
+            <div className="hexagon">
+              <div className="hexagon-in1">
+                <img src="http://placekitten.com/100/200" className="hexagon-in2" />
+              </div>
+            </div>
+          </Tilt>
+        </div>
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer">
+          <Tilt className="tilt">
+            <div className="hexagon">
+              <div className="hexagon-in1">
+                <img src="http://placekitten.com/100/200" className="hexagon-in2" />
+              </div>
+            </div>
+          </Tilt>
+        </div>
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer">
+          <Tilt className="tilt">
+            <div className="hexagon">
+              <div className="hexagon-in1">
+                <img src="http://placekitten.com/100/200" className="hexagon-in2" />
+              </div>
+            </div>
+          </Tilt>
+        </div>
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer">
+          <Tilt className="tilt">
+            <div className="hexagon">
+              <div className="hexagon-in1">
+                <img src="http://placekitten.com/100/200" className="hexagon-in2" />
+              </div>
+            </div>
+          </Tilt>
+        </div>
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer">
+          <Tilt className="tilt">
+            <div className="hexagon">
+              <div className="hexagon-in1">
+                <img src="http://placekitten.com/100/200" className="hexagon-in2" />
+              </div>
+            </div>
+          </Tilt>
+        </div>
+        <div className="navigationItemContainer" />
+        <div className="navigationItemContainer" />
+      </div>
     </div>
   );
 };
